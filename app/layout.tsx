ebc98de13653c0938a39424f9f78c33eba106c81
boaloysius <boaloysius@gmail.com>
2024-01-24 22:12:39 +0530

    add assets and layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,27 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "A community driven platform to ask and resolve questions.",
+  icons: { icon: "/assets/images/site-logo.svg" },
+};
 
 export default function RootLayout({
   children,
@@ -8,9 +29,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">Hello there</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
