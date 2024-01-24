3c4d88f1fce460d3b6a67c476750dfb6d6269115
boaloysius <boaloysius@gmail.com>
2024-01-24 21:48:24 +0530

    add auth

---
+++ app/(home)/ask-question/page.tsx
@@ -0,0 +1,3 @@
+export default function AskQuestion() {
+  return <div className="h-screen">Hello there</div>;
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Boby Aloysius Johnson</h1>
-      <h1 className="h2-bold">Boby Aloysius Johnson</h1>
-      <h1 className="h3-bold">Boby Aloysius Johnson</h1>
-    </div>
-  );
-}
